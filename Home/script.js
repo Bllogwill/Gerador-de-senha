85fc8c4193d416ee76bcc5adb6d22baeee05b8ab
William Silva <tintilho@yahoo.com.br>
2022-07-17 22:34:25 +0100

    Projeto3

---
+++ Home/script.js
@@ -0,0 +1,32 @@
+let sliderElement = document.querySelector('#slider')
+let buttonElement = document.querySelector('#Button')
+
+let sizePassword = document.querySelector('#valor')
+let password = document.querySelector('#password')
+
+let containerPassword = document.querySelector('#container-password')
+
+let charset = 'qwertyyuuiopaskdjfg;zxbcvnm1234558900-5850'
+let novaSenha = ''
+
+sizePassword.innerHTML = sliderElement.value
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value
+}
+
+function generatePassword() {
+  let pass = ''
+
+  for (let i = 0; i < sliderElement.value; i++) {
+    pass += charset.charAt(Math.floor(Math.random() * i))
+  }
+
+  containerPassword.classList.remove('hide')
+  password.innerHTML = pass
+}
+
+function copyPassword(){
+  alert("Senha Copiada com sucesso!")
+  navigator.clipboard.writeText(novasenha)
+}
